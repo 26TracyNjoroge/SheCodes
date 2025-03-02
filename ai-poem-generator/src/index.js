79fed8cd1a4775bb37d647189576ca5d8bec4b40
Tracy Njoroge <tracywangari997@gmail.com>
2025-03-02 17:11:45 +0300

    Add the AI API intergration and the page responsiveness

--- ai-poem-generator/src/index.js
+++ ai-poem-generator/src/index.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
 
     new Typewriter('#response', {
-        strings: 'Hey there, things will be alright',
+        strings: response.data.answer,
         autoStart: true,
         cursor: "",
         delay: 100,
       });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionInput = document.querySelector("#user-instructions");
+    let apiKey = "db90o24154efb0eb442t9a7ef39fdafd";
+    let prompt = `Generate a poem about ${instructionInput.value}`;
+    let context = "You are a poem expert who loves writing short poems. Your task is to generate a four-short-line poem in basic HTML while following the user's instructions. Do not include quotes or any additional HTML outside the poem. Each line should be separated with a <br/>. At the end of the poem, sign it with 'SheCodes AI' inside a <strong> element.Do not mention HTML.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+
 }
 
 let formEl = document.querySelector("#ai-poem-form");
