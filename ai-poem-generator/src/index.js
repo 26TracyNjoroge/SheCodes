d2c4eb6c2cc010c0aae3b176754f93e432f897ea
Tracy Njoroge <tracywangari997@gmail.com>
2025-03-02 21:58:56 +0300

    Improve user experience

--- ai-poem-generator/src/index.js
+++ ai-poem-generator/src/index.js
@@ -14,10 +14,20 @@ function generatePoem(event) {
     let instructionInput = document.querySelector("#user-instructions");
     let apiKey = "db90o24154efb0eb442t9a7ef39fdafd";
     let prompt = `Generate a poem about ${instructionInput.value}`;
-    let context = "You are a poem expert who loves writing short poems. Your task is to generate a four-short-line poem in basic HTML while following the user's instructions. Do not include quotes or any additional HTML outside the poem. Each line should be separated with a <br/>. At the end of the poem, sign it with 'SheCodes AI' inside a <strong> element.Do not mention HTML.";
+    let context = "You are a poem expert who loves writing short poems. Your task is to generate a six-short-line poem in basic HTML while following the user's instructions. Do not include quotes or any additional HTML outside the poem. Each line should be separated with a <br/>. At the end of the poem, sign it with 'SheCodes AI' inside a <strong> element.Do not mention HTML.";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(displayPoem);
+
+    let responseEl = document.querySelector(".response-area");
+    responseEl.classList.remove("hidden");
+
+    new Typewriter('#response', {
+        strings: "Generating...",
+        autoStart: true,
+        cursor: "",
+        delay: 10,
+      });
 
 }
 
