07dcc25e0165ff4563b411294d232a52aae112fd
Tracy Njoroge <tracywangari997@gmail.com>
2025-03-10 15:29:54 +0300

    Add the current time for the respective time zones

--- world-clock/src/index.js
+++ world-clock/src/index.js
@@ -1,1 +1,24 @@
-alert('Hey!')
+function updateTime() {
+    //Nairobi
+    let nairobiElement = document.querySelector("#nairobi");
+    let nairobiDateElement = nairobiElement.querySelector(".date");
+    let nairobiTimeElement = nairobiElement.querySelector(".time");
+    
+    let nairobiCurrentTime = moment().tz("Africa/Nairobi");
+    
+    nairobiDateElement.innerHTML = nairobiCurrentTime.format("MMMM Do YYYY");
+    nairobiTimeElement.innerHTML = nairobiCurrentTime.format("H:mm:ss [<small>]A[</small>]")  
+
+    //Abuja
+    let lagosElement = document.querySelector("#lagos");
+    let lagosDateElement = lagosElement.querySelector(".date");
+    let lagosTimeElement = lagosElement.querySelector(".time");
+    
+    let lagosCurrentTime = moment().tz("Africa/Lagos");
+    
+    lagosDateElement.innerHTML = lagosCurrentTime.format("MMMM Do YYYY");
+    lagosTimeElement.innerHTML = lagosCurrentTime.format("H:mm:ss [<small>]A[</small>]")  
+}
+
+updateTime();
+setInterval(updateTime, 1000);
