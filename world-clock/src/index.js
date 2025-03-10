6ff758846b70651c77d785521042d5cc52519359
Tracy Njoroge <tracywangari997@gmail.com>
2025-03-10 16:54:50 +0300

    Add the select element functionality

--- world-clock/src/index.js
+++ world-clock/src/index.js
@@ -1,24 +1,52 @@
 function updateTime() {
     //Nairobi
     let nairobiElement = document.querySelector("#nairobi");
-    let nairobiDateElement = nairobiElement.querySelector(".date");
-    let nairobiTimeElement = nairobiElement.querySelector(".time");
-    
-    let nairobiCurrentTime = moment().tz("Africa/Nairobi");
-    
-    nairobiDateElement.innerHTML = nairobiCurrentTime.format("MMMM Do YYYY");
-    nairobiTimeElement.innerHTML = nairobiCurrentTime.format("H:mm:ss [<small>]A[</small>]")  
+    if (nairobiElement) {
+        let nairobiDateElement = nairobiElement.querySelector(".date");
+        let nairobiTimeElement = nairobiElement.querySelector(".time");
+        
+        let nairobiCurrentTime = moment().tz("Africa/Nairobi");
+        
+        nairobiDateElement.innerHTML = nairobiCurrentTime.format("MMMM Do YYYY");
+        nairobiTimeElement.innerHTML = nairobiCurrentTime.format("H:mm:ss [<small>]A[</small>]")  
+    }
+   
 
     //Abuja
     let lagosElement = document.querySelector("#lagos");
-    let lagosDateElement = lagosElement.querySelector(".date");
-    let lagosTimeElement = lagosElement.querySelector(".time");
+    if (lagosElement) {
+        let lagosDateElement = lagosElement.querySelector(".date");
+        let lagosTimeElement = lagosElement.querySelector(".time");
+        
+        let lagosCurrentTime = moment().tz("Africa/Lagos");
+        
+        lagosDateElement.innerHTML = lagosCurrentTime.format("MMMM Do YYYY");
+        lagosTimeElement.innerHTML = lagosCurrentTime.format("H:mm:ss [<small>]A[</small>]")  
+    }
     
-    let lagosCurrentTime = moment().tz("Africa/Lagos");
-    
-    lagosDateElement.innerHTML = lagosCurrentTime.format("MMMM Do YYYY");
-    lagosTimeElement.innerHTML = lagosCurrentTime.format("H:mm:ss [<small>]A[</small>]")  
+}
+
+function updateCity(event) {
+    let city = event.target.value;
+    let cityName = city.replace("_", " ").split("/")[1];
+    let cityTimeZone =moment().tz(city);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = `
+    <div class="city">
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTimeZone.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTimeZone.format("H:mm:ss")} <small>${cityTimeZone.format("A")}</small></div>
+    </div>
+    `
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let countryEl = document.querySelector("#coutry");
+countryEl.addEventListener("change", updateCity);
+
+
